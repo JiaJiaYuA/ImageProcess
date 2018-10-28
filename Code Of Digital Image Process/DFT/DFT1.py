5e53388e6faa598772a0dca34032d229a85343f5
JiaJiaYuA <yfx_xj@outlook.com>
2018-10-28 22:42:09 +0800

    DFT2

--- Code Of Digital Image Process/DFT/DFT1.py
+++ Code Of Digital Image Process/DFT/DFT1.py
@@ -19,25 +19,44 @@ img1 = readImage('woman.tif')
 
 #matlab
 f1 = np.fft.fft2(img1)
+f1Value = np.abs(f1)
+f1Angle = np.angle(f1)
+
+iImgV = np.zeros(img1.shape, dtype = complex)
+#angleImg
+iImgV.real = np.array(np.cos(f1Angle))
+iImgV.imag = np.array(np.sin(f1Angle))
+iImgAngle = np.fft.ifft2(iImgV)
+#valueImg
+iImgV.real = np.array(f1Value*np.cos(0))
+iImgV.imag = np.array(f1Value*np.sin(0))
+iImgValue = np.fft.ifft2(iImgV)
+#iImgTotal
+iImgV.real = np.array(f1Value*np.cos(f1Angle))
+iImgV.imag = np.array(f1Value*np.sin(f1Angle))
+iImgTotal = np.fft.ifft2(iImgV)
+
 #fshift1 = np.fft.fftshift(f1)
-fshiftAngle1 = np.angle(f1)
+#fshiftAngle1 = np.angle(f1)
 #f2shiftAngle1 = np.fft.ifftshift(fshiftAngle1)
 
-s1 = (np.abs(f1))
-siuint64=np.uint64(np.abs(f1))
-s1Uint8 = np.uint8(s1)
+#s1 = (np.abs(f1))
+#siuint64=np.uint64(np.abs(f1))
+#s1Uint8 = np.uint8(s1)
 #s1_real = s1*np.cos(fshiftAngle1)
 #s1_imag = s1*np.sin(fshiftAngle1)
-s2 = np.zeros(img1.shape,dtype=complex) 
-s2.real = s1#np.array(s1_real)
-s2.imag = 0#np.array(s1_imag)
+#s2 = np.zeros(img1.shape,dtype=complex) 
+#s2.real = s1#np.array(s1_real)
+#s2.imag = 0#np.array(s1_imag)
 
-imgBack = np.fft.ifft2(s2)
+#imgBack = np.fft.ifft2(s2)
 
 
-displayImg(121,img1,'a')
-displayImg(122,s1,'b')
-#displayImg(122,np.abs(imgBack),'b')
+displayImg(151,img1,'f1')
+displayImg(152,np.uint8(f1Value),'b')
+displayImg(153,np.abs(iImgAngle),'c')
+displayImg(154,np.abs(iImgValue),'d')
+displayImg(155,np.abs(iImgTotal),'e')
 plt.show()
 
 #opencv
